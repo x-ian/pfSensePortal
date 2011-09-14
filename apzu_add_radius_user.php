03a7c11d77b4b7db042ec64149f49eba89f11bd2
x-ian <x-ian@x-ian.net>
2011-09-14 21:32:09 +0200

    nix

--- apzu_add_radius_user.php
+++ apzu_add_radius_user.php
@@ -1,7 +1,25 @@
-<?php
-	$ip=$_SERVER['REMOTE_ADDR'];
-	$name=$_REQUEST['name'];
-	$email=$_REQUEST['email'];
-        echo "Trying to register" . $name . $email . $ip;
-	echo exec("/home/pfSensePortal/add_user_to_radius.sh " . $ip . " " . $name . " " . $email);
-?>
+<span style="font-variant:small-caps; font-size:200%">
+	<p align="center">
+		Abwenzi Pa Za Umoyo / Partners In Health
+	</p>
+</span>
+<table>
+	<tr>
+		<td>
+			<img src="apzu.png" />
+		</td>
+		<td>
+			Everybody gets connected - Hopefully you as well!<br/>
+			<p><?php
+				$ip=$_SERVER['REMOTE_ADDR'];
+				$name=$_REQUEST['name'];
+				$email=$_REQUEST['email'];
+			    echo "The registration process should be done by now for " . $name . " (" . $email . ") from " . $ip;
+				echo exec("/home/pfSensePortal/add_user_to_radius.sh " . $ip . " " . $name . " " . $email);
+			?>
+			</p>
+			<p>If you are not able to access any webpages like <a href="http://www.google.mw">Google</a>, please find the APZU-IT team.</p>
+		</td>
+	</tr>
+</table>
+
