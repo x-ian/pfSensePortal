d1b1fe6ad496b92765b6c245ce800757bb6d6e8f
x-ian <x-ian@x-ian.net>
2011-09-15 09:16:27 +0200

    logging, rae conditions

--- register_user.sh
+++ register_user.sh
@@ -8,6 +8,7 @@ NAME=$2
 EMAIL=$3
 NETBIOS=$4
 
+LOGFILE=/var/log/apzu-portal.log
 BASEDIR=`dirname $0`
 TODAY=`date +%Y-%m-%d`
 DESCRIPTION=`echo "$NAME; $EMAIL; $NETBIOS; $TODAY" | sed -f $BASEDIR/urlencode.sed`
@@ -17,29 +18,32 @@ SERVER=https://172.16.1.2
 PFSENSE_RADIUS_PASSWD=pfSense
 SESSIONTIME=3600
 
+TEMP_COOKIES=`mktemp /tmp/wget.cookies.XXXXXX`
+TEMP_OUTFILE=`mktemp /tmp/wget.outfile.XXXXXX`
+
 # Tget pfSense login details
 source $BASEDIR/credentials.config
 
 # login
 URL="login=Login&usernamefld=`echo $USER`&passwordfld=`echo $PASSWD`"
-OUTFILE=pf_login.html
-wget --keep-session-cookies --save-cookies cookies.txt  --post-data $URL --no-check-certificate $SERVER -O $OUTFILE
+wget --keep-session-cookies --save-cookies $TEMP_COOKIES --post-data $URL --no-check-certificate $SERVER -O $TEMP_OUTFILE
 
-# get next free user id, maybe not necessary to always recalc the current highest number
+# TODO?, get next free user id, maybe not necessary to always recalc the current highest number
 NEW_USER_ID=4
 
 # get page with csrf token
 URL="pkg_edit.php?xml=freeradius.xml&id=$NEW_USER_ID"
-OUTFILE=pf_add_user.html
-wget  --keep-session-cookies --load-cookies cookies.txt --no-check-certificate "$SERVER/$URL" -O $OUTFILE
+wget --keep-session-cookies --load-cookies $TEMP_COOKIES --no-check-certificate "$SERVER/$URL" -O $TEMP_OUTFILE
 
 # parse csrf token
-CSRF=`grep name=\'__csrf_magic\' pf_add_user.html  | sed "s/^.*value=\"//g" | sed "s/\".*$//g" | sed "s/:/%3A/g" | sed "s/,/%2C/g"`
+CSRF=`grep name=\'__csrf_magic\' pf_add_user.html | sed "s/^.*value=\"//g" | sed "s/\".*$//g" | sed "s/:/%3A/g" | sed "s/,/%2C/g"`
 
 # place request
 URL="pkg_edit.php?xml=freeradius.xml&id=$NEW_USER_ID"
-OUTFILE=pf_add_user_2.html
 POST="__csrf_magic=`echo $CSRF`&xml=freeradius.xml&username=`echo $MAC`&password=`echo $PFSENSE_RADIUS_PASSWD`&ip=&multiconnet=1&expiration=&sessiontime=`echo $SESSIONTIME`&onlinetime=&description=`echo $DESCRIPTION`&vlanid=&additionaloptions=`echo $ADDITIONAL_OPTIONS`&id=$NEW_USER_ID&Submit=Save"
-wget --keep-session-cookies --load-cookies cookies.txt  --no-check-certificate --post-data $POST "$SERVER/$URL" -O $OUTFILE
+wget --keep-session-cookies --load-cookies $TEMP_COOKIES --no-check-certificate --post-data $POST "$SERVER/$URL" -O $TEMP_OUTFILE
 
 # TODO, cleanup & logging
+echo "Register system `date`; $MAC; $DESCRIPTION">> $LOGFILE
+rm -f $TEMP_COOKIES
+rm -f $TEMP_OUTFILE
