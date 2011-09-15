6cfd3600b4dc14d4bcdedca919e9eb4009eca8a5
x-ian <x-ian@x-ian.net>
2011-09-15 11:21:35 +0200

    sd

--- resolve_mac_address.sh
+++ resolve_mac_address.sh
@@ -1,7 +1,9 @@
 #!/usr/local/bin/bash
 
+# there might be a clevererer way, maybe use mac straight from portal
+
 IP=$1
 
-MAC=$(ping -t 2 $IP > /dev/null && arp $IP | awk '{print $4}' | sed s/://g)
+MAC=$(ping -t 2 $IP > /dev/null; arp $IP | awk '{print $4}' | sed s/://g)
 
 echo $MAC
