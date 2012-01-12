dcd8ed8de403f8615ffffd14ae7403255d91f0ea
x-ian <x-ian@x-ian.net>
2012-01-12 10:24:20 +0100

    days ago tweaking

--- create_accounting_summary_csv.sh
+++ create_accounting_summary_csv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# takes the radius account information to summaries this in a complete csv file
+# requires http://www.tummy.com/Community/software/radiuscontext/
+
 TODAY=`date +%Y-%m-%d`
 ACCOUNTING_CSV=accounting_`echo $TODAY`.csv
 ACCOUNTING_DIR=accountingdir
@@ -7,19 +10,19 @@ RADIUS_DIR=radiusdir
 REPORT_DIR=reportdir
 #PFSENSE_CONFIG=/conf/config.xml
 PFSENSE_CONFIG=./config.xml
-MAX_DAYS_BACK=8
+MAX_DAYS_BACK=7
 
 # --------------------------------------
-# invoke radiusContext for every detail file
+# invoke radiusContext for last 7 detail file
 # todo, make incremental updates
-#for RADIUS_DETAIL in `ls $RADIUS_DIR`; do
-#  ./radiusContext-1.93/raddetail $RADIUS_DIR/$RADIUS_DETAIL
-#  ./radiusContext-1.93/stdreport --generate csv -g csv --reportdir $REPORT_DIR
-#  mv $REPORT_DIR/index.csv $ACCOUNTING_DIR/index_$RADIUS_DETAIL.csv
-#  # todo, cleanup of radiusContext files, there should be a better way but i didn't look for one
-#  rm -rf $REPORT_DIR/*
-#  rm -f SessionData.db
-#done
+for RADIUS_DETAIL in `ls -r -1 $RADIUS_DIR | head -n $MAX_DAYS_BACK`; do
+  ./radiusContext-1.93/raddetail $RADIUS_DIR/$RADIUS_DETAIL
+  ./radiusContext-1.93/stdreport --generate csv -g csv --reportdir $REPORT_DIR
+  mv $REPORT_DIR/index.csv $ACCOUNTING_DIR/index_$RADIUS_DETAIL.csv
+  # todo, cleanup of radiusContext files, there should be a better way but i didn't look for one
+  rm -rf $REPORT_DIR/*
+  rm -f SessionData.db
+done
 
 # --------------------------------------
 # get all ever registered mac addresses and users
@@ -57,11 +60,11 @@ done
 # export to complete data set to one csv
 # create header
 HEADER="MAC,MAC_DETAILS"
-for (( i=1; i <=$DAYS_AGO; i++ )); do
+for (( i=1; i <=$MAX_DAYS_AGO; i++ )); do
   HEADER="`echo $HEADER`,OUT -$i days"
 done
 HEADER="`echo $HEADER`,separator"
-for (( i=1; i <=$DAYS_AGO; i++ )); do
+for (( i=1; i <=$MAX_DAYS_AGO; i++ )); do
   HEADER="`echo $HEADER`,IN -$i days"
 done
 echo $HEADER >$ACCOUNTING_CSV
@@ -70,12 +73,12 @@ for MAC in `cat mac_addresses.txt`; do
   # MAC_DETAILS are specific to APZUs registration process and may or may not be available
   MAC_DETAILS=`grep -A 10 $MAC $PFSENSE_CONFIG | grep description | sed -e "s/<description><\!\[CDATA\[//g" | sed -e "s/\]\]><\/description>//g" | tr -d "\t" | tr -d " "`
   LINE="$MAC,$MAC_DETAILS"
-  for (( i=1; i <=$DAYS_AGO; i++ )); do
+  for (( i=1; i <=$MAX_DAYS_AGO; i++ )); do
 	OUT=$(eval echo \$A_${MAC}_${i}_OUT)
 	LINE=`echo $LINE,$OUT`
   done
   LINE="`echo $LINE`,separator"
-  for (( i=1; i <=$DAYS_AGO; i++ )); do
+  for (( i=1; i <=$MAX_DAYS_AGO; i++ )); do
 	IN=$(eval echo \$A_${MAC}_${i}_IN)
 	LINE=`echo $LINE,$IN`
 #	eval echo \$${MAC}_${DAYS_AGO}_IN
@@ -85,3 +88,4 @@ for MAC in `cat mac_addresses.txt`; do
 done
 
 rm mac_addresses.txt
+
