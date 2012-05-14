e74a8ced9459b89d86ad41141c2842316a537cdd
xian <x-ian@x-ian.net>
2012-05-14 19:27:41 +0200

    squid housekeeping hopefully done right

--- weekly_maintenance.sh
+++ weekly_maintenance.sh
@@ -8,10 +8,16 @@
 echo BIG_TIME_TODO
 
 # compacting squid cache (http://doc.pfsense.org/index.php/Squid_Package_Tuning)
-/usr/local/sbin/squid -k rotate
+#/usr/local/sbin/squid -k rotate
+
+# clearing out and recreating the whole squid cache dir
+/usr/local/sbin/squid -k shutdown
+/bin/sleep 10
+/bin/rm -rf /var/squid/cache/*
+/usr/local/sbin/squid -z
 
 # some internal backup
-/home/pfSensePortal/download_backup.sh
+#/home/pfSensePortal/download_backup.sh
 
 # just for the case, restart once in a while
 /sbin/reboot
