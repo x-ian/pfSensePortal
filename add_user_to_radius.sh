dc3852908ce6e26697f6462f9f84e66a829a8d8d
x-ian <x-ian@x-ian.net>
2012-06-19 13:13:19 +0200

    attempt to avoid double-registration

--- add_user_to_radius.sh
+++ add_user_to_radius.sh
@@ -8,9 +8,19 @@ OWNER=$4
 BASEDIR=`dirname $0`
 
 MAC=$($BASEDIR/resolve_mac_address.sh $IP)
-# doesn't seem as reliable as dhcp hostname
-#NETBIOS=$($BASEDIR/resolve_netbios_name.sh $IP)
-DHCPHOSTNAME=$($BASEDIR/resolve_hostname.sh $IP)
 
-$BASEDIR/register_user.sh $MAC "$NAME" "$EMAIL" $IP $OWNER "$DHCPHOSTNAME"
-perl -I /usr/local/lib/perl5/site_perl/5.10.1/ -I /usr/local/lib/perl5/site_perl/5.10.1/mach $BASEDIR/send_gmail.perl "$MAC" "$NAME" "$EMAIL" "$IP" "$OWNER" "$DHCPHOSTNAME"
+# check if we already have this user/MAC address and ignore if necessary
+# sometimes after restarting it seems that the Captive Portal redirects to the register page, 
+# even though the radius entry already exists
+PFSENSE_CONFIG=/conf/config.xml
+grep "$MAC" $PFSENSE_CONFIG
+if [ $? -ne 0 ]; then
+  # no entry found, continue to register
+
+  # netbios doesn't seem as reliable as dhcp hostname
+  #NETBIOS=$($BASEDIR/resolve_netbios_name.sh $IP)
+  DHCPHOSTNAME=$($BASEDIR/resolve_hostname.sh $IP)
+
+  $BASEDIR/register_user.sh $MAC "$NAME" "$EMAIL" $IP $OWNER "$DHCPHOSTNAME"
+  perl -I /usr/local/lib/perl5/site_perl/5.10.1/ -I /usr/local/lib/perl5/site_perl/5.10.1/mach $BASEDIR/send_gmail.perl "$MAC" "$NAME" "$EMAIL" "$IP" "$OWNER" "$DHCPHOSTNAME"
+fi
