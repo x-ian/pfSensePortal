e7703cc46d65b6c12a69b83cfd333cff0a596f41
Christian Neumann <x-ian@x-ian.net>
2013-08-10 11:45:44 +0200

    basedir fix

--- add_user_to_radius.sh
+++ add_user_to_radius.sh
@@ -11,7 +11,7 @@ MAC=$($BASEDIR/resolve_mac_address.sh $IP)
 MAC_FIRST_DIGITS=$(echo $MAC | cut -c 1-6 | awk '{print toupper($0)}')
 # check in the local copy of the IEEE OUI database
 # once in a while get an update from http://standards.ieee.org/develop/regauth/oui/public.html
-MAC_VENDOR=$(grep "(base 16)" ieee_oui.txt | grep $MAC_FIRST_DIGITS | awk -F"\t" '{ print $3 }')
+MAC_VENDOR=$(grep "(base 16)" $BASEDIR/ieee_oui.txt | grep $MAC_FIRST_DIGITS | awk -F"\t" '{ print $3 }')
 
 # check if we already have this user/MAC address and ignore if necessary
 # sometimes after restarting it seems that the Captive Portal redirects to the register page, 
