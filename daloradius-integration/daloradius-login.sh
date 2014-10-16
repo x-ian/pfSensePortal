457f207e38230b2040fd6130fbf12f65d1748044
Christian Neumann <x-ian@x-ian.net>
2014-10-16 12:43:51 +0200

    adjusted pathes

--- daloradius-integration/daloradius-login.sh
+++ daloradius-integration/daloradius-login.sh
@@ -6,4 +6,4 @@ BASEDIR=`dirname $0`
 source $BASEDIR/../config.txt
 PATH=$PATH:/usr/local/bin
 
-curl -c cookies.txt --url $DR_SERVER/daloradius/dologin.php" --user $DR_HTTP_USER_PASSWD --data "operator_user=$DR_USER&operator_pass=$DR_PASSWD&location=default"
+curl -c cookies.txt --url "$DR_SERVER/daloradius/dologin.php" --user $DR_HTTP_USER_PASSWD --data "operator_user=$DR_USER&operator_pass=$DR_PASSWD&location=default"
--- daloradius-integration/daloradius-new-user-with-mac-auth.sh
+++ daloradius-integration/daloradius-new-user-with-mac-auth.sh
@@ -16,6 +16,6 @@ NOTES=$7
 
 $BASEDIR/daloradius-login.sh
 
-curl -b cookies.txt --url $DR_SERVER/daloradius/mng-new.php" --user $DR_HTTP_USER_PASSWD --data "authType=macAuth&macaddress=$MAC&group_macaddress%5B%5D=$GROUP&firstname=$FIRSTNAME&lastname=$LASTNAME&email=$EMAIL&department=&company=$COMPANY&workphone=&homephone=&mobilephone=&address=&city=&state=&country=&zip=&notes=$NOTES&portalLoginPassword=&submit=Apply&bi_contactperson=&bi_company=&bi_email=&bi_phone=&bi_address=&bi_city=&bi_state=&bi_country=&bi_zip=&bi_postalinvoice=&bi_faxinvoice=&bi_emailinvoice=&bi_paymentmethod=&bi_cash=&bi_creditcardname=&bi_creditcardnumber=&bi_creditcardverification=&bi_creditcardtype=&bi_creditcardexp=&bi_lead=&bi_coupon=&bi_ordertaker=&bi_notes=&bi_billdue=&bi_nextinvoicedue="
+curl -b cookies.txt --url "$DR_SERVER/daloradius/mng-new.php" --user $DR_HTTP_USER_PASSWD --data "authType=macAuth&macaddress=$MAC&group_macaddress%5B%5D=$GROUP&firstname=$FIRSTNAME&lastname=$LASTNAME&email=$EMAIL&department=&company=$COMPANY&workphone=&homephone=&mobilephone=&address=&city=&state=&country=&zip=&notes=$NOTES&portalLoginPassword=&submit=Apply&bi_contactperson=&bi_company=&bi_email=&bi_phone=&bi_address=&bi_city=&bi_state=&bi_country=&bi_zip=&bi_postalinvoice=&bi_faxinvoice=&bi_emailinvoice=&bi_paymentmethod=&bi_cash=&bi_creditcardname=&bi_creditcardnumber=&bi_creditcardverification=&bi_creditcardtype=&bi_creditcardexp=&bi_lead=&bi_coupon=&bi_ordertaker=&bi_notes=&bi_billdue=&bi_nextinvoicedue="
 
 rm cookies.txt
