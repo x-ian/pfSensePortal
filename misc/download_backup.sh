01edc1397687753975b4b3de5b086befbd8d10ec
Christian Neumann <x-ian@x-ian.net>
2014-10-24 10:34:16 +0200

    autoomatic download

--- misc/download_backup.sh
+++ misc/download_backup.sh
@@ -3,23 +3,22 @@
 # remotely download latest backup file
 
 BASEDIR=/home/pfSensePortal
-SERVER=https://172.16.1.2
 CONFIG_FILE=config-router-`date +%Y%m%d%H%M%S`.xml
 
 # get pfSense login details
-source $BASEDIR/credentials.config
+source $BASEDIR/config.txt
 
 # login
 wget -qO/dev/null --keep-session-cookies --save-cookies cookies.txt \
  --post-data "login=Login&usernamefld=`echo $USER`&passwordfld=`echo $PASSWD`" \
- --no-check-certificate $SERVER/diag_backup.php
+ --no-check-certificate http://$IP/diag_backup.php
 
 # get the stuff
 wget --keep-session-cookies --load-cookies cookies.txt \
- --post-data 'Submit=download&donotbackuprrd=yes' $SERVER/diag_backup.php \
+ --post-data 'Submit=download&donotbackuprrd=yes' $IP/diag_backup.php \
  --no-check-certificate -O $CONFIG_FILE
 
-scp /home/pfSensePortal/$CONFIG_FILE backup@dev.pih-emr.org:malawi/pfsense
-mv /home/pfSensePortal/$CONFIG_FILE /home/pfSensePortal/backup_sequence
+#scp /home/pfSensePortal/$CONFIG_FILE backup@dev.pih-emr.org:malawi/pfsense
+#mv /home/pfSensePortal/$CONFIG_FILE /home/pfSensePortal/backup_sequence
 
 
