c1b6b6bf3bcbbd2efd51b20770d0d680440aef38
Christian Neumann <x-ian@x-ian.net>
2014-10-26 19:18:20 +0100

    finalized radius return codes

--- daloradius-integration/captive-portal-check_device_status.sh
+++ daloradius-integration/captive-portal-check_device_status.sh
@@ -21,23 +21,23 @@ echo $RADTEST | grep "Reply-Message"
 CHECK_REPLY=$?
 echo $RADTEST | grep "Access-Reject"
 CHECK_ACCESS=$?
-if [ $CHECK_REPLY -eq 1 ] && [ $CHECK_ACCESS -eq 0]; then
+if [ $CHECK_REPLY -eq 1 ] && [ $CHECK_ACCESS -eq 0 ]; then
   echo "no reply message, assume device not yet registered"
   echo "$MAC $IP - 1 - device not yet registered - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
   exit 1
 fi
 
-echo $RADTEST | grep "Reply-Message = \"Too many users. Please try again later\""
+echo $RADTEST | grep "Too many users"
 if [ $? -eq 0 ]; then
   echo "Too many users. Please try again later."
   echo "$MAC $IP - 2 - Too many users - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
   exit 2
 fi
 
-echo $RADTEST | grep "Reply-Message = \"Over quota.\""
+echo $RADTEST | grep "usage time has been reached"
 if [ $? -eq 0 ]; then
-  echo "Over quota."
-  echo "$MAC $IP - 3 - Over quota - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
+  echo "Data volume reached"
+  echo "$MAC $IP - 3 - Data volume reached - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
   exit 3
 fi
 
@@ -54,3 +54,4 @@ fi
 echo "RADIUS server offline or unknown response. Please contact the IT team."
 echo "$MAC $IP - 9 - RADIUS offline or unknown response - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
 exit 9
+
