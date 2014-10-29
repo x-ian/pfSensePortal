63a30fad0254a3a36b850225067544c8cc6b940b
Christian Neumann <x-ian@x-ian.net>
2014-10-29 15:09:06 +0200

    imported config

--- daloradius-integration/captive-portal-add_user_to_radius.sh
+++ daloradius-integration/captive-portal-add_user_to_radius.sh
@@ -8,6 +8,7 @@ OWNER=$4
 DATE=`date +%Y%m%d-%H%M`
 
 BASEDIR=`dirname $0`
+source $BASEDIR/../config.txt
 
 MAC=$($BASEDIR/resolve_mac_address.sh $IP)
 MAC_FIRST_DIGITS=$(echo $MAC | cut -c 1-6 | awk '{print toupper($0)}')
