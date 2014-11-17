5a3847adc8250184acc15e6a3bfc11e9a6137558
Christian Neumann <x-ian@x-ian.net>
2014-11-17 21:04:04 +0100

    added check for data bundle during busines hours

--- daloradius-integration/captive-portal-check_device_status.sh
+++ daloradius-integration/captive-portal-check_device_status.sh
@@ -15,36 +15,44 @@ echo $RADTEST | grep "Access-Reject"
 CHECK_ACCESS=$?
 if [ $CHECK_REPLY -eq 1 ] && [ $CHECK_ACCESS -eq 0 ]; then
   echo "no reply message, assume device not yet registered"
-  echo "$MAC - $IP - 1 - device not yet registered - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
+  echo "$MAC - $IP - 1 - device not yet registered - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
   exit 1
 fi
 
 echo $RADTEST | grep "Too many users"
 if [ $? -eq 0 ]; then
   echo "Too many users. Please try again later."
-  echo "$MAC - $IP - 2 - Too many users - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
+  echo "$MAC - $IP - 2 - Too many users - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
   exit 2
 fi
 
 echo $RADTEST | grep "usage time has been reached"
 if [ $? -eq 0 ]; then
   echo "Data volume reached"
-  echo "$MAC - $IP - 3 - Data volume reached - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
+  echo "$MAC - $IP - 3 - Data volume reached - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
   exit 3
 fi
 
 echo $RADTEST | grep "Your device is permanently disabled"
 if [ $? -eq 0 ]; then
   echo "Device permanently disabled."
-  echo "$MAC - $IP - 4 - Device permanently disabled - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
+  echo "$MAC - $IP - 4 - Device permanently disabled - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
   exit 4
+fi
+
+echo $RADTEST | grep "Data bundle during business hours exceeded"
+if [ $? -eq 0 ]; then
+  echo "Data bundle during business hours exceeded."
+  echo "$MAC - $IP - 5 - Data bundle during business hours exceeded - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
+  exit 5
 fi
 
 echo $RADTEST | grep "Access-Accept"
 if [ $? -eq 0 ]; then
   # will not happen for calls from captive portal as this will already send an auth packet before
+  # simply adding it here for completeness
   echo "device enabled and active"
-  echo "$MAC - $IP - 0 - enabled and active - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
+  echo "$MAC - $IP - 0 - enabled and active - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
   exit 0
 fi
 
@@ -52,6 +60,6 @@ fi
 # e.g. "no response from server"
 # e.g. "Failed to find IP address for pfsense.apzunet"
 echo "RADIUS server offline or unknown response. Please contact the IT team."
-echo "$MAC - $IP - 9 - RADIUS offline or unknown response - `date +%Y%m%d-%H%M%S`" >> /tmp/check_device_status.log
+echo "$MAC - $IP - 9 - RADIUS offline or unknown response - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
 exit 9
 
