a1b282be73fdfae62d0f19334f8c4eae5fc652e8
Christian Neumann <x-ian@x-ian.net>
2014-11-18 09:40:36 +0100

    better log file for device statuses

--- daloradius-integration/captive-portal-check_device_status.sh
+++ daloradius-integration/captive-portal-check_device_status.sh
@@ -5,6 +5,8 @@ IP=$1
 BASEDIR=`dirname $0`
 source $BASEDIR/../config.txt
 
+STATUS_LOG=/home/device_status_log/status-`date date +%Y%m%d`.log
+
 MAC=$($BASEDIR/resolve_mac_address.sh $IP)
 
 RADTEST=$(radtest $MAC radius $DR_IP 0 radius) >/dev/null
@@ -16,7 +18,7 @@ CHECK_ACCESS=$?
 
 if [ $CHECK_REPLY -eq 1 ] && [ $CHECK_ACCESS -eq 0 ]; then
   echo "no reply message, assume device not yet registered"
-  echo "$MAC - $IP - 1 - device not yet registered - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
+  echo "$MAC - $IP - 1 - device not yet registered - `date +%Y%m%d-%H%M%S`" >> $STATUS_LOG
   exit 1
 fi
 
@@ -24,14 +26,14 @@ echo $RADTEST | grep "Too many users" >/dev/null
 if [ $? -eq 0 ]; then
   echo $RADTEST | grep Reply-Message | sed 's/.*Reply-Message = //g'
   echo "Too many users. Please try again later."
-  echo "$MAC - $IP - 2 - Too many users - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
+  echo "$MAC - $IP - 2 - Too many users - `date +%Y%m%d-%H%M%S`" >> $STATUS_LOG
   exit 2
 fi
 
 echo $RADTEST | grep "usage time has been reached" >/dev/null
 if [ $? -eq 0 ]; then
   echo "Data volume reached"
-  echo "$MAC - $IP - 3 - Data volume reached - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
+  echo "$MAC - $IP - 3 - Data volume reached - `date +%Y%m%d-%H%M%S`" >> $STATUS_LOG
   exit 3
 fi
 
@@ -39,7 +41,7 @@ echo $RADTEST | grep "Your device is permanently disabled" >/dev/null
 if [ $? -eq 0 ]; then
   echo $RADTEST | grep Reply-Message | sed 's/.*Reply-Message = //g'
   echo "Device permanently disabled."
-  echo "$MAC - $IP - 4 - Device permanently disabled - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
+  echo "$MAC - $IP - 4 - Device permanently disabled - `date +%Y%m%d-%H%M%S`" >> $STATUS_LOG
   exit 4
 fi
 
@@ -47,7 +49,7 @@ echo $RADTEST | grep "Data bundle during business hours exceeded" >/dev/null
 if [ $? -eq 0 ]; then
   echo $RADTEST | grep Reply-Message | sed 's/.*Reply-Message = //g'
   echo "Data bundle during business hours exceeded."
-  echo "$MAC - $IP - 5 - Data bundle during business hours exceeded - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
+  echo "$MAC - $IP - 5 - Data bundle during business hours exceeded - `date +%Y%m%d-%H%M%S`" >> $STATUS_LOG
   exit 5
 fi
 
@@ -56,7 +58,7 @@ if [ $? -eq 0 ]; then
   # will not happen for calls from captive portal as this will already send an auth packet before
   # simply adding it here for completeness
   echo "device enabled and active"
-  echo "$MAC - $IP - 0 - enabled and active - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
+  echo "$MAC - $IP - 0 - enabled and active - `date +%Y%m%d-%H%M%S`" >> $STATUS_LOG
   exit 0
 fi
 
@@ -65,6 +67,6 @@ fi
 # e.g. "Failed to find IP address for pfsense.apzunet"
 echo "RADIUS server offline or unknown response. Please contact the IT team."
 echo $RADTEST
-echo "$MAC - $IP - 9 - RADIUS offline or unknown response - `date +%Y%m%d-%H%M%S`" >> /home/check_device_status.log
+echo "$MAC - $IP - 9 - RADIUS offline or unknown response - `date +%Y%m%d-%H%M%S`" >> $STATUS_LOG
 exit 9
 
