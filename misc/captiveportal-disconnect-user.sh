e984bd413b6d209dae2d322a84c2be3058181f6c
Christian Neumann <x-ian@x-ian.net>
2014-11-18 13:07:54 +0100

    integration into freeradius

--- misc/captiveportal-disconnect-user.sh
+++ misc/captiveportal-disconnect-user.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-# remotely download latest backup file
-
-BASEDIR=.. #/home/pfSensePortal
+BASEDIR=/home/pfSensePortal
 MAC=$1
+
+TMP_COOKIES=`mktemp`-cookies.txt
+TMP_ALL=`mktemp`-all.html
+TMP_ALL2=`mktemp`-all2.html
 
 # get pfSense login details
 source $BASEDIR/config.txt
 
 # login
-wget -qO/dev/null --keep-session-cookies --save-cookies cookies.txt \
+wget -qO/dev/null --keep-session-cookies --save-cookies $TMP_COOKIES \
  --post-data "login=Login&usernamefld=`echo $USER`&passwordfld=`echo $PASSWD`" \
  --no-check-certificate $PF_SERVER/diag_backup.php
 
 # get all active users
-wget --keep-session-cookies --load-cookies cookies.txt --no-check-certificate --output-document=all.html \
+wget --keep-session-cookies --load-cookies $TMP_COOKIES --no-check-certificate --output-document=$TMP_ALL \
   "$PF_SERVER/status_captiveportal.php?zone=$ZONE&order=&showact=&act=del&id=4c72b958c5d2dee1"
 
 # loop over users and terminate all sessions
 # should only be one, but I cloned the disconnect-all-users script
-cat all.html | grep -A3 `echo $MAC` | tail -1  | cut -d "\"" -f2 | while read -r url
+cat $TMP_ALL | grep -A3 `echo $MAC` | tail -1  | cut -d "\"" -f2 | while read -r url
 do
-  wget --keep-session-cookies --load-cookies cookies.txt --no-check-certificate  --output-document=all2.html \
+  wget --keep-session-cookies --load-cookies $TMP_COOKIES --no-check-certificate  --output-document=$TMP_ALL2 \
     "$PF_SERVER/status_captiveportal.php$url"
 done
 
-rm all.html
-rm all2.html
-rm cookies.txt
+rm $TMP_ALL
+rm $TMP_ALL2
+rm $TMP_COOKIES
 
 
